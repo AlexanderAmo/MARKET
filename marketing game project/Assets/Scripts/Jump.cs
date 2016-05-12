6aa02d4de754d4532d9eab8172da9c19c55032e0
Gamerdude12321 <tomaskaplaniscoll@gmail.com>
2016-05-12 13:22:44 -0700

    Adding Movement

---
+++ marketing game project/Assets/Scripts/Jump.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Jump : MonoBehaviour {
+
+	public float jumpSpeed = 240f;
+	public float forwardSpeed = 20;
+	public float jumpCount = 0;
+
+	public float walkSpeed = 100;
+
+
+	private Rigidbody2D body2d;
+	private InputState inputState;
+	private Animator animator;
+
+	void Awake(){
+		body2d = GetComponent<Rigidbody2D> ();
+		inputState = GetComponent<InputState> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var running = false;
+		var jumping = false;
+		var running2 = false;
+		if (inputState.standing) {
+
+
+			if (inputState.actionButton) {
+				body2d.velocity = new Vector2 (forwardSpeed , jumpSpeed);
+				jumpCount++;
+				body2d.velocity = new Vector2 (forwardSpeed , jumpSpeed);
+			}
+			if (inputState.actionButton2) {
+				body2d.velocity = new Vector2 (forwardSpeed , jumpSpeed * 1.5f);
+				jumpCount++;
+				body2d.velocity = new Vector2 (forwardSpeed , jumpSpeed * 1.5f);
+			}
+		}
+			if (inputState.leftArrow) {
+				body2d.velocity = new Vector2 (walkSpeed * -1 , 0);
+
+				body2d.velocity = new Vector2 (walkSpeed * -1 , 0);
+				//animator.SetBool ("Running", running);
+			}
+			if (inputState.rightArrow) {
+				body2d.velocity = new Vector2 (walkSpeed , 0);
+
+				body2d.velocity = new Vector2 (walkSpeed , 0);
+				//animator.SetBool ("Running", running);
+		
+		}
+	}
+}
---
+++ marketing game project/Assets/Scripts/PlayerAnimationManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationManager : MonoBehaviour {
+
+	private Animator animator;
+	private InputState inputState;
+
+	// Use this for initialization
+	void Awake () {
+	
+		animator = GetComponent<Animator> ();
+		inputState = GetComponent<InputState> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		var running = false;
+		var running2 = false;
+		var jumping = false;
+
+		if(inputState.rightArrow = true){
+			running = true;
+			//animator.SetBool ("Running", running);
+		}
+		if (inputState.leftArrow = true) {
+			running = true;
+			//animator.SetBool ("Running", running);
+		}
+		if(inputState.actionButton = true){
+			jumping = true;
+			//animator.SetBool ("Jumping", jumping);
+		}
+		if (inputState.actionButton2 = true) {
+			jumping = true;
+			//animator.SetBool ("Jumping", jumping);
+		}
+		//animator.SetBool ("Running", running);
+		//animator.SetBool ("Jumping", jumping);
+
+
+	}
+}
