8c32f4006850463e4b1339259112c292b1048189
goodudegood <goodudegood1@gmail.com>
2016-05-19 11:51:04 -0700

    animations

---
+++ marketing game project/Assets/Scripts/animatorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animatorController : MonoBehaviour {
+
+	public static animatorController instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
