735387c3ce0f8ee1a78eb2c0116434eee80c5f99
goodudegood <goodudegood1@gmail.com>
2016-05-19 13:24:20 -0700

    awed

--- marketing game project/Assets/Scripts/PlayerController.cs
+++ marketing game project/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 	
 	public float speed = 10f, jumpVelocity = 10f;
 	public LayerMask playermask;
 	Transform myTrans, TagGround;
 	Rigidbody2D myRigid;
-
 	public bool isGrounded = false;
+	animatorController myAnim;
+	float hInput;
 
 	// Use this for initialization
 	void Start () {
 		myRigid = this.GetComponent<Rigidbody2D> ();
 		myTrans = this.transform;
 		TagGround = GameObject.Find (this.name + "/tag_ground").transform;
+		myAnim = animatorController.instance;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		isGrounded = Physics2D.Linecast (myTrans.position, TagGround.position,playermask);
+		myAnim.UpdateIsGrounded (isGrounded);
 
-
-		move (Input.GetAxisRaw("Horizontal"));
-		if (Input.GetButtonDown ("Jump")) {
+		hInput = Input.GetAxisRaw("Horizontal");
+		myAnim.UpdateSpeed (hInput);
+		if (Input.GetButtonDown ("Jump")) 
 			jump();
-		}
+
+		move (hInput);
 	}
 
 	public void move(float HorizontalInput){
@@ -38,6 +42,11 @@ public class PlayerMovement : MonoBehaviour {
 		if (isGrounded) {
 			myRigid.velocity += jumpVelocity * Vector2.up;
 		}
+	}
+	public void startMoving(float horizonalInput){
+		hInput = horizonalInput;
+		myAnim.UpdateSpeed (hInput);
+
 	}
 
 }
--- marketing game project/Assets/Scripts/animatorController.cs
+++ marketing game project/Assets/Scripts/animatorController.cs
@@ -5,13 +5,39 @@ public class animatorController : MonoBehaviour {
 
 	public static animatorController instance;
 
+	Transform myTrans;
+	Animator myAnim;
+	Vector3 artScaleCache;
+
+
 	// Use this for initialization
 	void Start () {
 		instance = this;
+		myTrans = this.transform;
+		myAnim = this.gameObject.GetComponent<Animator>();
+
+		artScaleCache = myTrans.localScale;
 	}
-	
+
+
+	void FlipArt(float currentSpeed){
+		if ((currentSpeed < 0 && artScaleCache.x == 1) ||
+			(currentSpeed > 0 && artScaleCache.x == -1)) {
+			artScaleCache.x*= -1;
+			myTrans.localScale = artScaleCache;
+		}
+	}
+
+	public void UpdateSpeed(float currentSpeed){
+		myAnim.SetFloat ("Speed", currentSpeed);
+		FlipArt (currentSpeed);
+	}
+
+	public void UpdateIsGrounded(bool isGrounded) {
+		myAnim.SetBool ("IsGrounded", isGrounded);
+	}
 	// Update is called once per frame
 	void Update () {
-	
+				
 	}
 }
